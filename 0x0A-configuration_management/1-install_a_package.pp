53663582f3891410b3e30fad59686bc4b6e4ca28
Damilola <akinpentium@gmail.com>
2023-03-28 19:15:22 -0700

    redo

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,11 @@
-# Installs puppet-lint, version 2.1.0
-#
-  package { 'puppet-lint':
-  ensure => '2.1.0',
+package { 'python3-pip':
+  ensure => installed,
 }
 
 exec { 'install-flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path => '/usr/local/bin:/usr/bin:/bin',
-  creates => '/usr/local/lib/puppet-lint/dist-packages/flask',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
 }
+
 
