016f68e65354d141164ae41e79f146c4ffb82735
Damilola <akinpentium@gmail.com>
2023-03-28 19:25:24 -0700

    tesf

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,7 +8,7 @@ package { 'pupet-lint':
 exec { 'install-flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path => '/usr/local/bin:/usr/bin:/bin',
-  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+  creates => '/usr/local/lib/puppet-lint/dist-packages/flask',
 }
 
 
